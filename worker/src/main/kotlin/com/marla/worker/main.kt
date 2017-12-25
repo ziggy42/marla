711a95451feec5db3fdebacf43dc0f54b0f3326d
AndreaPivetta <vanpivix@gmail.com>
2017-12-25 19:31:03 +0100

    Add monkey dependency to worker

--- worker/src/main/kotlin/com/marla/worker/main.kt
+++ worker/src/main/kotlin/com/marla/worker/main.kt
@@ -1,5 +1,11 @@
 package com.marla.worker
 
+import monkey.`object`.Environment
+import monkey.ast.Parser
+import monkey.evaluator.Evaluator
+import monkey.lexer.StringLexer
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    val program = Parser(StringLexer("3 + 3")).parseProgram()
+    println(Evaluator.eval(program, Environment()).inspect())
 }
