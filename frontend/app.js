97d573fe7aa3db69f4648e052860bb2c0d9290e6
AndreaPivetta <vanpivix@gmail.com>
2018-01-14 21:28:39 +0100

    Configure all the things

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
-const app = express()
+const pug = require("pug")
 
 const PORT = process.env.PORT || 3000
+const WS_ENDPOINT = process.env.WS_ENDPOINT || "ws://localhost:8090/stomp"
+const API_ENDPOINT = process.env.API_ENDPOINT || "http://localhost:8080/api/job"
 
-app.use(express.static("public"))
+const app = express()
+
+app.get("/", (req, res) => res.send(pug.renderFile("public/index.pug", {
+    WS_ENDPOINT, API_ENDPOINT
+})))
 
 app.get("/health", (req, res) => res.send("ok"))
 
--- frontend/public/js/websocket.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-    const messageList = $("#messages")
-    const stompClient = Stomp.client("ws://localhost:8090/stomp")
-    stompClient.connect({}, () => {
-        stompClient.subscribe("/Andrea", (data) => messageList.append(`<li>${data.body}</li>`))
-    })
-})
