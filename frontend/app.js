13adba06f04f3a74e27dd2343f7a93da94c1a0b6
AndreaPivetta <vanpivix@gmail.com>
2018-01-14 21:04:40 +0100

    Frontend port can be defined by env variable

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.static("public"))
 
 app.get("/health", (req, res) => res.send("ok"))
 
-app.listen(3000, () => console.log("Frontend listening on port 3000!"))
+app.listen(PORT, () => console.log(`Frontend listening on port ${PORT}!`))
