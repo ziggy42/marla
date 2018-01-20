5846b1afc6dc4df014f6023e3cb943a7123d491c
AndreaPivetta <vanpivix@gmail.com>
2018-01-20 23:32:55 +0100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit
+fi
+VERSION=${1}
+
 MODULES=("api" "frontend" "websocket" "worker")
 for MODULE in "${MODULES[@]}"; do
-    docker tag marla/${MODULE} repo.treescale.com/ziggy42/marla/${MODULE}
-    docker push repo.treescale.com/ziggy42/marla/${MODULE}
+    docker tag marla/${MODULE} repo.treescale.com/ziggy42/marla/${MODULE}:${VERSION}
+    docker push repo.treescale.com/ziggy42/marla/${MODULE}:${VERSION}
 done
