919f92eb69be9e2812eeb336fa4825f5db8d8bd9
AndreaPivetta <vanpivix@gmail.com>
2018-02-18 22:02:49 +0100

    Add missing bean RestTemplate

--- api/src/main/kotlin/com/marla/api/main.kt
+++ api/src/main/kotlin/com/marla/api/main.kt
@@ -1,9 +1,12 @@
 package com.marla.api
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.marla.api.config.RedisConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.client.RestTemplate
 import redis.clients.jedis.JedisPool
 
 @SpringBootApplication
@@ -11,6 +14,14 @@ class MarlaApiApplication {
 
     @Bean
     fun jedisPool(configuration: RedisConfiguration): JedisPool = JedisPool(configuration.host, configuration.portValue)
+
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplate().apply {
+        val messageConverter = MappingJackson2HttpMessageConverter()
+        messageConverter.setPrettyPrint(false)
+        messageConverter.objectMapper = ObjectMapper()
+        messageConverters.add(messageConverter)
+    }
 }
 
 fun main(args: Array<String>) {
