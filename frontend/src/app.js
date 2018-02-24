ea614150d8fe4f4c24ebe8e60d9bafb291b0faf8
AndreaPivetta <vanpivix@gmail.com>
2018-02-24 19:04:35 +0100

    Forward errors to the client

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -8,7 +8,9 @@ app.use(express.json())
 
 app.get("/", (req, res) => res.send(pug.renderFile(`${__dirname}/../public/index.pug`, { WS_ENDPOINT })))
 
-app.post("/job", (req, res) => postJob(req.body, req.headers).then((response) => res.send(response)))
+app.post("/job", (req, res) => postJob(req.body, req.headers)
+    .then((response) => res.send(response))
+    .catch((err) => res.send(err)))
 
 app.get("/health", (req, res) => res.send("ok"))
 
