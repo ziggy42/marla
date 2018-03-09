15d9d5e585ce217bc885f8aa546af734ba9bfe1b
AndreaPivetta <vanpivix@gmail.com>
2018-03-09 20:00:59 +0100

    Remove the :monkey: :broken_heart:

--- worker/monkey/src/main/kotlin/monkey/object/ObjectType.kt
+++
@@ -1,9 +0,0 @@
-package monkey.`object`
-
-/**
- * @author andrea
- * @since 7/29/17
- */
-enum class ObjectType {
-    INTEGER, STRING, BOOLEAN, NULL, RETURN_VALUE, ERROR, FUNCTION, BUILTIN, ARRAY, HASH
-}
--- worker/monkey/src/main/kotlin/monkey/token/Token.kt
+++
@@ -1,7 +0,0 @@
-package monkey.token
-
-/**
- * @author andrea
- * @since 7/17/17
- */
-data class Token(val tokenType: TokenType, val literal: String)
